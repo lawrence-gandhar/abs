16df00502dd25229ac5da0f5203fb3ad767a5951
lawrence-gandhar <lawrence_gandhar@yahoo.com>
2020-09-14 19:02:56 +0530

    updated

--- abshaadi/app/urls.py
+++ abshaadi/app/urls.py
@@ -26,17 +26,14 @@ urlpatterns += [
 # Profile
 urlpatterns += [
     path('profile/', never_cache(login_required(profiles_filters.UserProfileView.as_view())), name="user_profile"),
+    path('edit/<int:id>', never_cache(login_required(profiles_filters.UserProfileEdit.as_view())), name="user_edit"),
+    path('update/<int:id>', never_cache(login_required(profiles_filters.update)), name="user_update"),
+    
     path('upload_profile_pic/', never_cache(login_required(profiles_filters.upload_profile_pic)), name="upload_profile_pic"),
 ]
 
-# Package
-urlpatterns += [
-    path('staff/package_management/view/', never_cache(login_required(package_management.PackageManagementView.as_view())), name="staff_package_management_view"),
-    path('staff/edit_package_management/', never_cache(login_required(package_management.edit_package_management)), name="edit_package_management"),
-    path('staff/delete_package_management/<int:ins>/', never_cache(login_required(package_management.delete_package_management)), name="delete_package_management"),
-]
 
-# User Management
+# Package
 urlpatterns += [
     path('staff/user_management/users/view/', never_cache(login_required(user_management.UserManagementView.as_view())), name="user_management_view"),
     path('staff/user_management/users/delete/<int:ins>/', never_cache(login_required(user_management.delete_user)), name="delete_user"),
--- abshaadi/app/views/profiles_filters.py
+++ abshaadi/app/views/profiles_filters.py
@@ -10,7 +10,9 @@ from collections import defaultdict
 
 from app.models import * 
 from app.forms import *
-
+from django.shortcuts import get_object_or_404
+from app.forms.registration_forms import RegisterForm,ProfileForm
+from django.http import Http404
 
 #******************************************************************************
 # USER PROFILE VIEW
@@ -22,12 +24,12 @@ class UserProfileView(View):
     
     data = defaultdict()
 
-    data["included_template"] = 'app/users/profile.html'
+    data["included_template"] = 'app/users/profile1.html'
 
     data["page_title"] = "My Profile"
     
-    data["css_files"] = []
-    data["js_files"] = []
+    data["css_files"] = ['custom_files/css/Chart.min.css', 'custom_files/css/croppie.css']
+    data["js_files"] = ['custom_files/js/Chart.min.js', 'custom_files/js/croppie.js', 'custom_files/js/user_dashboard.js', 'custom_files/js/common.js']
     
     #
     #
@@ -38,18 +40,72 @@ class UserProfileView(View):
         try:
             profile_pic = ProfilePictures.objects.get(user=request.user, set_as_profile_pic=True)
             pro_pic = profile_pic.picture
+            
         except:
             pro_pic = ""
             pass
         
-        self.data["pro_pic"] = pro_pic        
+        self.data["pro_pic"] = pro_pic
         self.data["profile"] = Profile.objects.get(user=request.user)        
         self.data["gallery"] = ProfilePictures.objects.filter(user=request.user, set_as_profile_pic=False)
+        self.data['abc']=1
         
         
         return render(request, self.template_name, self.data)
+
+class UserProfileEdit(View):
+    template_name = 'app/base/base.html'
+    
+    data = defaultdict()
+
+    data["included_template"] = 'app/users/profile.html'
+    
+
+    data["page_title"] = "My Profile"
+    
+    data["css_files"] = ['custom_files/css/Chart.min.css', 'custom_files/css/croppie.css']
+    data["js_files"] = ['custom_files/js/Chart.min.js', 'custom_files/js/croppie.js', 'custom_files/js/user_dashboard.js', 'custom_files/js/common.js']
+    
+    def get(self,request,id):
+        try:
+            profile_pic = ProfilePictures.objects.get(user=request.user, set_as_profile_pic=True)
+            pro_pic = profile_pic.picture
+            
+        except:
+            pro_pic = ""
+            pass
         
+        self.data["pro_pic"] = pro_pic
+        self.data["profile"] = Profile.objects.get(user=request.user)        
+        self.data["gallery"] = ProfilePictures.objects.filter(user=request.user, set_as_profile_pic=False)
         
+        self.data['profile'] = Profile.objects.get(id=id)
+        
+        # print(profile.user)  
+        return render(request,self.template_name,self.data)
+
+
+        
+def update(request,id):
+    if request.method=='POST':
+        form =RegisterForm(request.POST) 
+        form1 =ProfileForm(request.POST)
+        # print(form1['father_name'].value())
+        obj = Profile.objects.get(id=id)
+        obj.fullname = form['fullname'].value()
+        obj.gender = form['gender'].value()
+        obj.looking_for_gender = form['looking_for_gender'].value()
+        obj.aged_from = form['aged_from'].value()
+        obj.aged_to = form['aged_to'].value()
+        obj.father_name=form1['father_name'].value()
+        obj.mother_name = form1['mother_name'].value()
+        obj.dob = form1['dob'].value()
+        obj.religion_id = form1['religion'].value()
+        obj.caste_creed_id = form1['caste_creed'].value()
+        obj.save()
+        return redirect('/profile')
+    
+    return render(request,'app/users/profile.html',{'profile':form1})
 
 #******************************************************************************
 # USER PROFILE VIEW
