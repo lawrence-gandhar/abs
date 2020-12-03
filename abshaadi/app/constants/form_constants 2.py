397819bff21899a84fd74bc58ff3977f0f12b59f
lawrence-gandhar <lawrence-gandhar@users.noreply.github.com>
2020-12-03 08:59:29 +0530

    updated

---
+++ abshaadi/app/constants/form_constants 2.py
@@ -0,0 +1,38 @@
+COMPLEXION = {
+    1 : "Light Skin",
+    2 : "Fair Skin",
+    3 : "Medium Skin",
+    4 : "Olive Skin",
+    5 : "Tan Brown Skin",
+    6 : "Black Brown Skin",
+}
+
+RELIGION = {
+    0 : "Any Religion",
+    1 : "Hindu",
+    2 : "Muslim",
+    3 : "Christian",
+    4 : "Sikh",
+    5 : "Jain",
+    6 : "Buddhist",
+    7 : "Jewish",
+}
+
+CASTE = {
+    0 : "Any Caste",
+    1 : "Brahmin",
+    2 : "Rajput",
+    3 : "Kurmi",
+    4 : "Yadav",
+    5 : "Paswan",
+    6 : "Koyari",
+    7 : "Mandal",
+    8 : "Kushwaha",
+    9 : "Harijan",
+    10 : "Kahar",
+    11 : "Sharma",
+    12 : "Bhumihar",
+    13 : "Rajak(Dhobi)",
+    14 : "Kumhar(Pandit)",
+}
+
