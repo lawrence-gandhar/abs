0c04721a68afe02e611eae77926b9bc11f037564
lawrence-gandhar <lawrence-gandhar@users.noreply.github.com>
2020-12-06 23:18:35 +0530

    updated

--- abshaadi/app/static/custom_files/js/common.js
+++ abshaadi/app/static/custom_files/js/common.js
@@ -427,3 +427,21 @@ $(function() {
 			});
 		}
 	}
+
+
+
+	$("#select_all").change(function(){  //"select all" change
+	    $(".checkbox").prop('checked', $(this).prop("checked")); //change all ".checkbox" checked status
+	});
+
+	//".checkbox" change
+	$('.checkbox').change(function(){
+		//uncheck "select all", if one of the listed checkbox item is unchecked
+	    if(false == $(this).prop("checked")){ //if this item is unchecked
+	        $("#select_all").prop('checked', false); //change "select all" checked status to false
+	    }
+		//check "select all" if all checkbox items are checked
+		if ($('.checkbox:checked').length == $('.checkbox').length ){
+			$("#select_all").prop('checked', true);
+		}
+	});
