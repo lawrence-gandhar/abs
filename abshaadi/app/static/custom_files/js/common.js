4fc9a4417e5c6e44b7298ac84c82a893c6887dfb
lawrence-gandhar <lawrence-gandhar@users.noreply.github.com>
2020-12-08 12:39:28 +0530

    updated

--- abshaadi/app/static/custom_files/js/common.js
+++ abshaadi/app/static/custom_files/js/common.js
@@ -429,6 +429,9 @@ $(function() {
 	}
 
 
+	//*******************************************************************************
+	// Multi Select Checkbox
+	//*******************************************************************************
 
 	$("#select_all").change(function(){  //"select all" change
 	    $(".checkbox").prop('checked', $(this).prop("checked")); //change all ".checkbox" checked status
@@ -437,9 +440,9 @@ $(function() {
 	//".checkbox" change
 	$('.checkbox').change(function(){
 		//uncheck "select all", if one of the listed checkbox item is unchecked
-	    if(false == $(this).prop("checked")){ //if this item is unchecked
-	        $("#select_all").prop('checked', false); //change "select all" checked status to false
-	    }
+    if(false == $(this).prop("checked")){ //if this item is unchecked
+        $("#select_all").prop('checked', false); //change "select all" checked status to false
+    }
 		//check "select all" if all checkbox items are checked
 		if ($('.checkbox:checked').length == $('.checkbox').length ){
 			$("#select_all").prop('checked', true);
--- abshaadi/app/static/custom_files/js/user_management.js
+++ abshaadi/app/static/custom_files/js/user_management.js
@@ -2,15 +2,18 @@ $(document).ready(function(){
 
 });
 
+//*******************************************************************************
+// DELETE USER
+//*******************************************************************************
 
 function delete_user(id){
 	$("#data_process_modal").modal('show');
-	
+
 	$.get('/staff/user_management/users/delete/'+id+'/', function(data){
 		if(data == 1){
-			localtion.reload();
+			location.reload();
 		}else{
-			$("#data_process_modal").modal('hide');	
+			$("#data_process_modal").modal('hide');
 			if(data == 0){
 				alert("Invalid operation");
 			}
@@ -21,21 +24,59 @@ function delete_user(id){
 	});
 }
 
+
+//*******************************************************************************
+// ADD STAFF
+//*******************************************************************************
+
 function add_staff(){
 	$("#data_process_modal").modal('show');
-	
+
 	$.post("/staff/user_management/staff/add/", $("#add_staff_form").serialize(), function(){
 		if(data==1){
 			location.reload();
 		}
 		else if(data==0){
-			$("#data_process_modal").modal('hide');	
+			$("#data_process_modal").modal('hide');
 			alert("In Valid Operation");
 		}
 		else{
-			$("#data_process_modal").modal('hide');	
+			$("#data_process_modal").modal('hide');
 			$("#error_modal").find(".modal-body").empty().append(data);
-			$("#error_modal").modal('show');	
+			$("#error_modal").modal('show');
 		}
 	});
+}
+
+//*******************************************************************************
+// USER EDIT MODAL
+//*******************************************************************************
+
+function user_edit_modal_form(id){
+
+	$("#user_id").val(id);
+
+	$("#user_edit_modal_form").modal('show');
+
+	$.post("{% url 'fetch_user_details' %}", {"user_id":id, "csrfmiddlewaretoken":csrfmiddlewaretoken}, function(data){
+
+		data = $.parseJSON(data);
+
+		$("#id_phone_number").val(data.phone_number);
+		$("#id_fullname").val(data.fullname);
+		$("#id_phone_number_alternative").val(data.phone_number_alternative);
+		$("#id_package").val(data.package);
+		$("#id_assigned_to").val(data.assigned_to);
+
+		console.log(data);
+	});
+}
+
+
+//*******************************************************************************
+// DELETE SELECTED USERS
+//*******************************************************************************
+
+function delete_selected_users(){
+	rr = confirm("Do you want to delete the selected users? All data will be lost for the selected users after this operation");
 }
